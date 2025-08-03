9ba5d7b2d44db26badbd6ad00bdf1443ca46118e
Dakshprajapat1212 <2023csdaksh13685@poornima.edu.in>
2025-08-03 16:45:08 +0530

    about to end

--- backend/src/controllers/message.controllers.js
+++ backend/src/controllers/message.controllers.js
@@ -34,37 +34,50 @@ export const getMessages=async(req,res)=>{
         res.status(500).json({ message: "Internal server error" });
     }
 }
-export const sendMessage= async(req,res)=>{
+export const sendMessage = async (req, res) => {
   try {
-    
-     const {text,image}= req.body;
-     console.log("dekh",req.body);
-     const {id:receiverId}=req.params;
-     const senderId=req.user._id;
-     console.log(" check user :",req.user)
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user?._id;
 
-     let imageUrl;
-     if(image){
+    console.log("Incoming message request:");
+    console.log("Sender:", senderId);
+    console.log("Receiver:", receiverId);
+    console.log("Text:", text);
+    console.log("Image:", image);
+
+    if (!senderId || !receiverId) {
+      return res.status(400).json({ message: "Missing sender or receiver ID" });
+    }
+
+    let imageUrl;
+    if (image) {
       const uploadResponse = await cloudinary.uploader.upload(image);
       imageUrl = uploadResponse.secure_url;
-     }
-     const newMessage = new Message({
-      senderId: senderId,
-      receiverId: receiverId,
-      text: text,
-      image: imageUrl
-     });
-     await newMessage.save();
-   const receiverSocketId = getReceiverSocketId(receiverId);
-    if (receiverSocketId) {
-    io.to(receiverSocketId).emit("newMessage", newMessage);
     }
-    if (myId) {
-    io.to(myId).emit("newMessage", newMessage);
-  }
 
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    const senderSocketId = getReceiverSocketId(senderId);
+
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+    if (senderSocketId) {
+      io.to(senderSocketId).emit("newMessage", newMessage);
+    }
+
+    res.status(201).json(newMessage);
   } catch (error) {
     console.error("Error sending message:", error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error", error: error.message });
   }
-}
+};
